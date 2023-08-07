939d983dfdd1335d8f655c642eca1b8cdfe93dcb
Mattcy <140085165+Mattcy1@users.noreply.github.com>
2023-08-07 10:07:28 +0200

    Add files via upload

---
+++ ModHelperData.cs
@@ -0,0 +1,15 @@
+﻿namespace CosmicMonkey
+{
+    public static class ModHelperData
+    {
+        public const string WorksOnVersion = "37.0";
+        public const string Version = "1.2";
+        public const string Name = "Cosmic Monkey";
+        public const string Description =
+            "Adds in a new custom tower themed around galaxy. Also Check Out My Youtube Channel Its MattcyYT with the litte orange and blue icon thanks!";
+
+        public const string RepoOwner = "Mattcy1";
+        public const string RepoName = "Cosmic-monkey";
+        public const string Icon = "CosmicMonkey-Icon.png";
+    }
+}
